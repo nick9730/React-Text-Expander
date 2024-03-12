ab9a25b7fa6d22e2db6807dac5f1fa9d6f479c88
nick9730 <nikos.koutrenis@gmail.com>
2024-03-12 06:16:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsed={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="red"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expand={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
     </div>
   );
 }
 
-export default App;
+function TextExpander({
+  children,
+  collapsed=60,
+  expand,
+  className='',
+  expandButtonText='Show more',
+  collapseButtonText= 'Show less',
+  buttonColor='yellow'
+
+}){
+ 
+const [show,setShow] = useState(expand);
+const [collapsedNumWords,setCollapseNumWords] = useState(children)
+  
+const expandButton = ()=>
+{
+  setShow(true)
+  setCollapseNumWords(children)
+}
+
+const collapseButton = ()=>{
+  setShow(false)
+  
+  setCollapseNumWords(children.slice(0,collapsed) + '...')
+}
+
+
+
+  return <div  className= {className}>
+         
+
+
+            {
+              show ? <div>
+                     {collapsedNumWords}
+                <button style= {{color:buttonColor}}onClick={collapseButton}>{collapseButtonText}</button> 
+              </div>
+              :
+              <div> 
+                {collapsedNumWords.slice(0,collapsed) + '....'}
+                <button style= {{color:buttonColor}} onClick={expandButton} >{expandButtonText}</button>
+              </div>
+            } 
+
+  
+  </div>;
+}
